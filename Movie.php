fce6383d0bb8871b7df9d625b339abaefbc68a6d
matteopelusi7 <pelusimatteo02@gmail.com>
2022-04-08 12:36:47 +0200

    Refactoring

--- Movie.php
+++ Movie.php
@@ -9,7 +9,7 @@ class Movie {
     public $genre;
     public $description;
 
-    function __construct($_title, $_author, $_vote, $_year, $_genre, $_description)
+    function __construct( string $_title, string $_author, int $_vote, int $_year, string $_genre, string $_description )
     {
         $this->title = $_title;
         $this->author = $_author;
@@ -21,6 +21,12 @@ class Movie {
 
     public function getFullMovie() {
         return "Titolo: $this->title <br> Autore: $this->author <br> Voto: $this->vote <br> Anno: $this->year <br> Genere: $this->genre <br> Descrizione: $this->description";
+    }
+
+    public function setTitle( string $_title ) {
+        if( !is_numeric($_title) ) {
+            $this->title = $_title;
+        }
     }
 
 }
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/Movie.php';
 
 $ritorno_al_futuro = new Movie("Ritorno al futuro", "Steven Spielberg", 9.5, 1985, "fantasy", "...");
 $jurassic_park = new Movie("Jurassic Park", "Steven Spielberg", 9.0, 1993, "fantasy", "...");
+$lo_squalo = new Movie("Lo Squalo", "Steven Spielberg", 8.8, 1975, "thriller", "...");
 
 var_dump($ritorno_al_futuro);
 var_dump($jurassic_park);
@@ -17,6 +18,11 @@ var_dump($jurassic_park);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>oop</title>
+    <style>
+        li {
+            padding: 20px 0;
+        }
+    </style>
 </head>
 <body>
 
@@ -26,6 +32,9 @@ var_dump($jurassic_park);
         </li>
         <li>
             <?php echo $jurassic_park->getFullMovie(); ?>
+        </li>
+        <li>
+            <?php echo $lo_squalo->getFullMovie(); ?>
         </li>
     </ul>
     
